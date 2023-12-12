5477debc1ed0165989bf1fe5bcfd562222ff74a9
Nilson Alcides <nilson_alcides@hotmail.com>
2023-12-12 20:46:47 -0300

    Class Tipo animal

---
+++ ClincaVeterinariaAspCore/Models/TipoAnimal.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace aspnetcoremvc_adonet.Models
+{
+    public class TipoAnimal
+    {
+        [Display(Name = "Código")]
+        public int Id { get; set; }
+
+        [Display(Name = "Tipo")]
+        public string Tipo { get; set; }
+    }
+}
