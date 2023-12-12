2600cee3a5f42f6e9103b268e03309861c9ef155
Nilson Alcides <nilson_alcides@hotmail.com>
2023-12-12 20:35:35 -0300

    Primeiro commit

---
+++ ClincaVeterinariaAspCore/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ClincaVeterinariaAspCore.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
