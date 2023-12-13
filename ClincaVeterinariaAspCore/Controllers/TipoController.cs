d406243b5f78a46519b4d1fb45a0cddac275fe15
Nilson Alcides <nilson_alcides@hotmail.com>
2023-12-12 21:05:47 -0300

    ClincaVeterinariaAspCore

---
+++ ClincaVeterinariaAspCore/Controllers/TipoController.cs
@@ -0,0 +1,56 @@
+﻿using aspnetcoremvc_adonet.Models;
+using aspnetcoremvc_adonet.Repositories.Contract;
+using Microsoft.AspNetCore.Mvc;
+
+namespace aspnetcoremvc_adonet.Controllers
+{
+    public class TipoController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private ITipoAnimalRepository _tipoAnimalRepository;
+
+        public TipoController(ILogger<HomeController> logger, ITipoAnimalRepository tipoAnimalRepository)
+        {
+            _logger = logger;
+            _tipoAnimalRepository = tipoAnimalRepository;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult ListaTipoAnimal()
+        {
+            return View(_tipoAnimalRepository.ObterTodosTipoAnimal());
+
+        }
+
+        public IActionResult CadTipo()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult CadTipo(TipoAnimal tipoAnimal)
+        {
+            _tipoAnimalRepository.Cadastrar(tipoAnimal);
+            ViewBag.Message = "Cadastro realizado com sucesso.";
+            return RedirectToAction(nameof(ListaTipoAnimal));
+        }
+
+        public IActionResult editarTipo(int id)
+        {
+
+            return View(_tipoAnimalRepository.ObterTipoAnimal(id));
+        }
+        [HttpPost]
+        public IActionResult editarTipo(TipoAnimal tipoAnimal)
+        {
+            _tipoAnimalRepository.Atualizar(tipoAnimal);
+            return RedirectToAction(nameof(ListaTipoAnimal));
+        }
+        public IActionResult Delete(int id)
+        {
+            _tipoAnimalRepository.Excluir(id);
+            return RedirectToAction(nameof(ListaTipoAnimal));
+        }
+    }
+}
