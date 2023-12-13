5ae425ad3908af40980edd78a5f011b2a938d2ce
Nilson Alcides <nilson_alcides@hotmail.com>
2023-12-12 22:12:49 -0300

    Veterinário interface

---
+++ ClincaVeterinariaAspCore/Repositories/Contract/IVeterinarioRepository.cs
@@ -0,0 +1,14 @@
+﻿using ClincaVeterinariaAspCore.Models;
+
+namespace ClincaVeterinariaAspCore.Repositories.Contract
+{
+    public interface IVeterinarioRepository
+    {
+        //CRUD
+        void Cadastrar(Veterinario veterinario);
+        void Atualizar(Veterinario veterinario);
+        void Excluir(int Id);
+        Veterinario ObterVeterinario(int Id);
+        IEnumerable<Veterinario> ObterTodosVeterinario();
+    }
+}
