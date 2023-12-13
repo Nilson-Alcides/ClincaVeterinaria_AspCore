5478ef64a7ccd747ab58622224b8d3de7b22475c
Nilson Alcides <nilson_alcides@hotmail.com>
2023-12-12 22:25:55 -0300

    Animal Model

---
+++ ClincaVeterinariaAspCore/Models/Animal.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ClincaVeterinariaAspCore.Models
+{
+    public class Animal
+    {
+        [Display(Name = "Código")]
+        public int Id { get; set; }
+
+        [Display(Name = "Animal")]
+        public string nomeAni { get; set; } 
+        
+        public Raca RefRaca { get; set; }
+
+        [Display(Name = "Raca")]
+        public List<Raca> ListaRaca { get; set; }
+
+        public Cliente RefCliente { get; set; }
+
+        [Display(Name = "Cliente")]
+        public List<Cliente> ListaCliente { get; set; }
+    }
+}
