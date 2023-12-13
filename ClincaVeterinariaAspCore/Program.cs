0fb8f0821593c56c9b95fec66c0ec749f5d687b7
Nilson Alcides <nilson_alcides@hotmail.com>
2023-12-12 23:37:39 -0300

    recursos para trabalhar com sessão

--- ClincaVeterinariaAspCore/Program.cs
+++ ClincaVeterinariaAspCore/Program.cs
@@ -1,10 +1,15 @@
 using ClincaVeterinariaAspCore.Repositories.Contract;
 using ClincaVeterinariaAspCore.Repositories;
+using ClincaVeterinariaAspCore.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
+
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddHttpContextAccessor();
+
 
 builder.Services.AddScoped<ITipoAnimalRepository, TipoAnimalRepository>();
 builder.Services.AddScoped<IRacaRepository, RacaRepository>();
@@ -14,6 +19,31 @@ builder.Services.AddScoped<IAnimalRepository, AnimalRepository>();
 builder.Services.AddScoped<IAtendimentoRepository, AtendimentoRepository>();
 builder.Services.AddScoped<ILoginRepository, LoginRepository>();
 
+builder.Services.Configure<CookiePolicyOptions>(options =>
+{
+
+    options.CheckConsentNeeded = context => true;
+    options.MinimumSameSitePolicy = SameSiteMode.None;
+});
+
+// corrigir problema com TEMPDATA
+builder.Services.AddDistributedMemoryCache();
+builder.Services.AddSession(options =>
+{
+    // Set a short timeout for easy testing. 
+    options.IdleTimeout = TimeSpan.FromSeconds(60);
+    options.Cookie.HttpOnly = true;
+    // Make the session cookie essential 
+    options.Cookie.IsEssential = true;
+});
+builder.Services.AddMvc().AddSessionStateTempDataProvider();
+
+builder.Services.AddMemoryCache(); // Guardar os dados na memoria
+builder.Services.AddSession(options =>
+{
+});
+
+builder.Services.AddScoped<Login>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -26,6 +56,7 @@ app.UseStaticFiles();
 app.UseRouting();
 
 app.UseAuthorization();
+app.UseSession();//Usar Sessão
 
 app.MapControllerRoute(
     name: "default",
