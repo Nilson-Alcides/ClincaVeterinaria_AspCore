8561ba181f8cc21c79c74d0b664cbb76f4240623
Nilson Alcides <nilson_alcides@hotmail.com>
2023-12-12 22:24:19 -0300

    Atendimento Model

---
+++ ClincaVeterinariaAspCore/Models/Atendimento.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ClincaVeterinariaAspCore.Models
+{
+    public class Atendimento
+    {
+        [Display(Name = "Codigo")]
+        public int Id { get; set; }
+
+        [Display(Name = "Data")]
+        public string dataAtend { get; set; }
+
+        [Display(Name = "Hora")]
+        public string horaAtend { get; set; }
+        
+        [Display(Name = "Confirmação")]
+        public string confAgendamento { get; set; }
+       
+        [Display(Name = "Status")]
+        public string statusAtend { get; set; }
+
+
+        [Display(Name = "Código Veterinario")]
+        public Veterinario RefVeterinario { get; set; }
+
+        public List<Veterinario> ListaVeterinario { get; set; }
+
+        [Display(Name = "Código Animal")]
+        public Animal RefAnimal { get; set; }
+
+        public List<Animal> ListaAnimal { get; set; }
+
+
+
+    }
+}
