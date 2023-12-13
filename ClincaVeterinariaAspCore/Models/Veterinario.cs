29d932df40960588edefbb56e8d8cd6598a0120f
Nilson Alcides <nilson_alcides@hotmail.com>
2023-12-12 22:11:38 -0300

    Model Veterinário

---
+++ ClincaVeterinariaAspCore/Models/Veterinario.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ClincaVeterinariaAspCore.Models
+{
+    public class Veterinario
+    {
+        [Display(Name = "Código")]
+        public int Id { get; set; }
+
+        [Display(Name = "Veterinario")]
+        public string NomeVeter { get; set; }
+
+        [Display(Name = "Telefone")]
+        public string TelVeter { get; set; }
+
+        [Display(Name = "E-mail")]
+        public string EmailVeter { get; set; }
+    }
+}
