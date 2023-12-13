bad3814398584571d140a5d5b5cfc56d8c246c5a
Nilson Alcides <nilson_alcides@hotmail.com>
2023-12-12 21:49:43 -0300

    Cliente Model

---
+++ ClincaVeterinariaAspCore/Models/Cliente.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ClincaVeterinariaAspCore.Models
+{
+    public class Cliente
+    {
+        [Display(Name = "Codigo")]
+        public int id { get; set; }
+
+        [Required(ErrorMessage = "O nome do cliente é obrigatorio")]
+        [Display(Name = "Nome")]
+        public string nomeCli { get; set; }
+
+        [Required(ErrorMessage = "O Telefone do cliente é obrigatorio")]
+        [Display(Name = "Telefone")]
+        public string telCli { get; set; }
+
+        [Required(ErrorMessage = "O E-mail do cliente é obrigatorio")]
+        [Display(Name = "E-mail")]
+        public string EmailCli { get; set; }
+
+    }
+}
