adb3fa16662b51289ec668aaccd86a053ddda810
Nilson Alcides <nilson_alcides@hotmail.com>
2023-12-12 22:27:06 -0300

    Animal interface

---
+++ ClincaVeterinariaAspCore/Repositories/Contract/IAnimalRepository.cs
@@ -0,0 +1,14 @@
+﻿using ClincaVeterinariaAspCore.Models;
+
+namespace ClincaVeterinariaAspCore.Repositories.Contract
+{
+    public interface IAnimalRepository
+    {
+        //CRUD
+        IEnumerable<Animal> ObterTodosAnimais();
+        void Cadastrar(Animal animal);
+        void Atualizar(Animal animal);
+        Animal ObterAnimais(int Id);        
+        void Excluir(int Id);
+    }
+}
