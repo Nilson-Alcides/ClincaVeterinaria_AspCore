948463a757a34382c5d8d862ad4254f280693206
Nilson Alcides <nilson_alcides@hotmail.com>
2023-12-12 21:51:09 -0300

    Cliente interface

---
+++ ClincaVeterinariaAspCore/Repositories/Contract/IClienteRepository.cs
@@ -0,0 +1,14 @@
+﻿using ClincaVeterinariaAspCore.Models;
+
+namespace ClincaVeterinariaAspCore.Repositories.Contract
+{
+    public interface IClienteRepository
+    {
+        //CRUD
+        void Cadastrar(Cliente cliente);
+        void Atualizar(Cliente cliente);
+        void Excluir(int Id);
+        Cliente ObterCliente(int Id);
+        IEnumerable<Cliente> ObterTodosClientes();
+    }
+}
