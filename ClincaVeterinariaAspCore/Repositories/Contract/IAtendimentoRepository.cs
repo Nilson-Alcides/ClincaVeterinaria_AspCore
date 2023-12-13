25ecca5050f4899df59ec722a36e0c9cf847453b
Nilson Alcides <nilson_alcides@hotmail.com>
2023-12-12 22:43:19 -0300

    Atendimento interface

---
+++ ClincaVeterinariaAspCore/Repositories/Contract/IAtendimentoRepository.cs
@@ -0,0 +1,23 @@
+﻿using aspnetcoremvc_adonet.Models;
+using System.Data;
+
+namespace aspnetcoremvc_adonet.Repositories.Contract
+{
+    public interface IAtendimentoRepository
+    {
+        //CRUD
+
+        IEnumerable<Atendimento> ObterTodosAtendimentos();
+        IEnumerable<Atendimento> ObterTodosAtendimentosDetalhes();
+        void TestarAgenda(Atendimento agenda);
+        void Cadastrar(Atendimento atendimento);
+        
+      //  void Atualizar(Atendimento atendimento);
+        Atendimento ObterAtendimentos(int Id);
+        void Excluir(int Id);
+
+        void CancelarAtend(Atendimento atendimento);
+        DataTable selecionaAgendaData(Atendimento atendimento);
+
+    }
+}
